bdce9a8d1bda2c034da6c6dbe0d639165dc867a8
Jim Knape <Jimwinthropknape@gmail.com>
2020-06-08 12:24:02 -0700

    Allows for only one play per cell and tracks player moves

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const cells = document.getElementsByClassName('play');
-
-console.log(cells)
+var moveNum = 0;
+var tracker = {
+    X: [[], [], []],
+    O: [[], [], []]
+}
 
 const playMove = function(event) {
-  event.target.textContent = "X";
-  console.log(event.target)
+  //find current player
+  (moveNum % 2 === 0) ? currentPlayer = "X" : currentPlayer = "O";
+  event.target.textContent = currentPlayer;
+
+  tracker[currentPlayer][event.target.parentNode.id].push(event.target.id)
+  console.log(tracker)
+  moveNum++;
 }
 
 

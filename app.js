563fcc963eb244f59cde8fdda36101f4b03437f3
Jim Knape <Jimwinthropknape@gmail.com>
2020-06-08 13:01:40 -0700

    Determines a winner and alerts players

--- app.js
+++ app.js
@@ -5,13 +5,34 @@ var tracker = {
     O: [[], [], []]
 }
 
+//create helper function to check for winner
+var checkWinner = function(currentPlayer) {
+  for (var i = 0; i < tracker[currentPlayer].length; i++) {
+    if (tracker[currentPlayer][i].length === 3) {
+      alert(`Player ${currentPlayer} wins!`);
+      //update game totals and reset board
+      return;
+    }
+  }
+  if (tracker[currentPlayer][1].indexOf(1) !== -1) {
+      if ((tracker[currentPlayer][0].indexOf(0) !== -1 && tracker[currentPlayer][2].indexOf(2) !== -1) || (tracker[currentPlayer][0].indexOf(2) !== -1 && tracker[currentPlayer][3].indexOf(0) !== -1)) {
+        alert(`Player ${currentPlayer} wins!`);
+        //update game totals and reset board?
+        return;
+      }
+    }
+    return;
+}
+
 const playMove = function(event) {
   //find current player
   (moveNum % 2 === 0) ? currentPlayer = "X" : currentPlayer = "O";
   event.target.textContent = currentPlayer;
-
-  tracker[currentPlayer][event.target.parentNode.id].push(event.target.id)
-  console.log(tracker)
+  tracker[currentPlayer][event.target.parentNode.id].push(Number(event.target.id))
+  //check for winner
+  if (moveNum >= 4) {
+      checkWinner(currentPlayer);
+  }
   moveNum++;
 }
 
